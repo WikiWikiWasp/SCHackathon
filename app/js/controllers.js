4ecf753bda97fe059b07b971056b28e0314dda41
katiejoysweaney <katiejoysweaney@gmail.com>
2017-04-07 16:13:09 -0500

    added heading

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,16 @@
 /**
  * Created by Jason on 4/7/2017.
  */
+
+angular.module('hackathon.controllers', []);
+
+var maritalStats = ["Single","Married","Separated","Divorced","Widowed"];
+var gender = ["Male","Female"];
+var ethnicity = ["African-American","American Indian / Alaska Native","Asian / Pacific Islander",
+    "Caucasian","Hispanic","Middle Eastern","Multi-Racial","Other","Decline to state"];
+var householdIncome = ["0 - 15,000", "15,001 - 29,000", "29,001 - 46,000", "46,001 and over"];
+var educationLevel = ["Less than high school", "GED", "High school graduate", "Some college", "Bachelor's degree",
+    "Greater than Bachelor's Degree"];
+var govAssitance = ["No", "Yes"];
+var typeOfVehile = ["Coupe", "Sedan", "SUV", "Truck", "Motorcycle", "Bicycle", "None"];
+var crimalHistory = ["No", "Yes"];
--- app/js/directives.js
+++ app/js/directives.js
@@ -2,15 +2,4 @@
  * Created by Jason on 4/7/2017.
  */
 
-var app = angular.module('store', []);
-
-var maritalStats = ["Single","Married","Separated","Divorced","Widowed"];
-var gender = ["Male","Female"];
-var ethnicity = ["African-American","American Indian / Alaska Native","Asian / Pacific Islander",
-                 "Caucasian","Hispanic","Middle Eastern","Multi-Racial","Other","Decline to state"];
-var householdIncome = ["0 - 15,000", "15,001 - 29,000", "29,001 - 46,000", "46,001 and over"];
-var educationLevel = ["Less than high school", "GED", "High school graduate", "Some college", "Bachelor's degree",
-                      "Greater than Bachelor's Degree"];
-var govAssitance = ["No", "Yes"];
-var typeOfVehile = ["Coupe", "Sedan", "SUV", "Truck", "Motorcycle", "Bicycle", "None"];
-var crimalHistory = ["No", "No"];
+angular.module('hackathon.directives', []);
