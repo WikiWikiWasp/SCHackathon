6e9ef2f3a5241df834da958110edc2288d4fa425
Micah Rowell <theforshizzalist@gmail.com>
2017-04-07 15:09:13 -0500

    Adding arrays for form input

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,9 @@
 /**
  * Created by Jason on 4/7/2017.
  */
+
+var maritalStats = ["Single","Married","Separated","Divorced","Widowed"];
+var gender = ["Male","Female"];
+var ethnicity = ["African-American","American Indian / Alaska Native","Asian / Pacific Islander",
+                 "Caucasian","Hispanic","Middle Eastern","Multi-Racial","Other","Decline to state"];
+var householdIncome = ["0 - 15,000", "15,001 - 29,000", "29,001 - 46,000", "46,001 and over"];
